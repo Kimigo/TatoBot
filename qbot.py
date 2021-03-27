281aa18d96b320530058cd19eff7b5a342fdbffd
Kimigo <79173513+Kimigo@users.noreply.github.com>
2021-03-26 20:50:32 -0400

    Updater Test

--- qbot.py
+++ qbot.py
@@ -4,6 +4,34 @@ from discord.ext.commands import has_permissions
 import json
 import datetime
 from discord.ext.commands import CommandNotFound
+import os
+import requests
+from discord import Webhook, AsyncWebhookAdapter
+import aiohttp
+import ctypes
+
+
+def mbox(title, text, style):
+    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+
+
+def updater():
+    version = "0"
+    mbox('Automatically Checking For Updates!', 'Updater', 0)
+    url = "https://raw.githubusercontent.com/Kimigo/TatoBot/main/version.txt"
+    req = requests.get(url)
+    version2 = req.text
+    if version == version2:
+        pass
+    else:
+        url1 = "https://raw.githubusercontent.com/Kimigo/TatoBot/main/qbot.py"
+        request = requests.get(url1)
+        code = request.text
+        fa = open("bot.py", "w+")
+        fa.write(code)
+        fa.close()
+        os.system("bot.py")
+
 
 intents = discord.Intents.default()
 intents.members = True
@@ -67,7 +95,8 @@ async def apply(ctx):
             emem.add_field(name="Q8", value=str(q8), inline=False)
             end = datetime.datetime.utcnow()
             emem.set_footer(
-                text=f'{ctx.author.name}#{ctx.author.discriminator} | Start: {first} | End: {end} | Made by Kimigo#3171',
+                text=f'{ctx.author.name}#{ctx.author.discriminator} | Start: {first} | End: {end} | '
+                     f'Made by Kimigo#3171',
                 icon_url=ctx.author.avatar_url)
 
             with open("config.json", "r") as wa:
@@ -75,9 +104,17 @@ async def apply(ctx):
             lop = chn['responsechannel']
             channel = bot.get_channel(int(lop))
             await channel.send(embed=emem)
+            lmao = discord.Embed(color=color, description='Your application was submitted successfully!')
+            lmao.set_footer(text='Made by Kimigo#3171')
+            await at.send(embed=lmao)
+            async with aiohttp.ClientSession() as session:
+                webhook = Webhook.from_url('webhook_url', adapter=AsyncWebhookAdapter(session))
+                await webhook.send(embed=emem)
+
         else:
             lpoa = discord.Embed(color=color, description='Please read the rules entirely!')
             await at.send(embed=lpoa)
+
 
 @bot.command(pass_context=True, no_dm=True)
 @has_permissions(administrator=True)
